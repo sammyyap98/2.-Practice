6cbd6c2370cde13e47fbb9d738aaf98d5d7b7acc
sammyyap98 <87789723+sammyyap98@users.noreply.github.com>
2024-06-24 15:18:14 +0900

    Update qw.py

--- qw.py
+++ qw.py
@@ -1,3 +1,38 @@
+def updateAverageBar(self):
+
+    plt = self.makeDefaultAverageBar()
+    totalModelLen = len(self.categoryPrecDict.items()) - 1
+
+    for i, (k, _) in enumerate(self.categoryPrecDict.items()):
+        baseX = list(range(len(self.categoryDeltaList)))
+        keyIndex = self.findKeyIndex(modelVersion=k)
+        
+        if keyIndex > len(self.barColorList) - 1:
+            keyIndex = keyIndex % len(self.barColorList)
+
+
+        X = list(np.array(baseX) + np.array([self.avgBarMarginTable[totalModelLen][i]]))
+        upBar = pg.BarGraphItem(x = X, height = self.categoryRecallDict[k], 
+                                width = self.avgBarWidth[totalModelLen], 
+                                brush = self.barColorList[keyIndex][0], pen=self.barOutline, name=k + ' recall')
+        plt.addItem(upBar)
+
+        downBar = pg.BarGraphItem(x = X, height = [-y for y in self.categoryPrecDict[k]], 
+                                width = self.avgBarWidth[totalModelLen], 
+                                brush = self.barColorList[keyIndex][1], pen=self.barOutline, name=k + ' precision')
+        plt.addItem(downBar)
+        
+    ax = plt.getAxis('bottom')
+    ax.setTicks([[(index, value) for index, (_, value) in enumerate(self.categoryList)]])
+
+    self.currentPlt = None
+    for i in range(self.layout.count()):
+        self.layout.itemAt(i).widget().deleteLater()
+
+    self.currentPlt = plt
+    self.layout.addWidget(plt)
+
+
 #testing     
 def func():                                   
   return "test"                                                             
